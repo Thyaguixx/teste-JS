a42709cc82c442fd20e18cb96438f29ed774cfec
Thyago Silveira <83200721+Thyaguixx@users.noreply.github.com>
2023-02-23 22:54:36 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const calculadora = (n1, n2, tipoConta) => {
+    if (tipoConta == 'soma'){
+        const resultado = n1 + n2;
+        console.log(`Resultado: ${resultado}`);
+    };
+
+    if (tipoConta == 'subtracao'){
+        const resultado = n1 - n2;
+        console.log(`Resultado: ${resultado}`);
+    };
+
+    if (tipoConta == 'multiplicacao'){
+        const resultado = n1 * n2;
+        console.log(`Resultado: ${resultado}`);
+    };
+
+    if (tipoConta == 'divisao'){
+        const resultado = n1 / n2;
+        console.log(`Resultado: ${resultado}`);
+    };
+};
